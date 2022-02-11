500772300f07eed1e3d05f680d26da8c50901c7a
BasiliskOps <noro.nero@gmail.com>
2022-02-10 20:47:57 -0700

    initiated main loop

--- version00.py
+++ version00.py
@@ -6,3 +6,10 @@ wins = 0
 losses = 0
 ties = 0
 #variables for score keeping
+
+#Main game loop
+while True:
+    print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
+    while True: # player input loop
+        print('Make your move: (r)ock (p)aper (s)cissors or (q)uit')
+        
